874bae2116c41cec378c03e169274fbffe39c21a
casinodarkwebex <66070204+casinodarkwebex@users.noreply.github.com>
2020-05-28 17:34:33 +0500

    Add files via upload

---
+++ precache-manifest.bd0d10d9c741e8c5158ed1b2a1c5c15d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffda9a7f17d9db15b01205affea8f441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "203b1a3dcfe6d6ff34d8",
+    "url": "/static/css/2.47f050ad.chunk.css"
+  },
+  {
+    "revision": "19ef1da07399d269edba",
+    "url": "/static/css/main.388fc7d1.chunk.css"
+  },
+  {
+    "revision": "203b1a3dcfe6d6ff34d8",
+    "url": "/static/js/2.27cd2fb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27cd2fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ef1da07399d269edba",
+    "url": "/static/js/main.1290b98f.chunk.js"
+  },
+  {
+    "revision": "e3880b40a7dac59c785e",
+    "url": "/static/js/runtime-main.5b6707ba.js"
+  },
+  {
+    "revision": "903c3a33d53f2ecd6e78f16fa5654108",
+    "url": "/static/media/coinmain.903c3a33.png"
+  },
+  {
+    "revision": "f086586d490ec7e9a29e2929168117d9",
+    "url": "/static/media/game.f086586d.png"
+  }
+]);
