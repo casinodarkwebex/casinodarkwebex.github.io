d31d73de5730608080da0d9e3e40022c4a33f1cc
casinodarkwebex <66070204+casinodarkwebex@users.noreply.github.com>
2020-11-10 13:50:05 +0500

    Add files via upload

---
+++ precache-manifest.da74e1512f8fdeac163da1aa1b204d94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c0254a5a4c09205c2276b167911085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f28cd097101b19ffc8da",
+    "url": "/static/css/2.6326594c.chunk.css"
+  },
+  {
+    "revision": "b391f5f333b85c75a416",
+    "url": "/static/css/main.76d648a5.chunk.css"
+  },
+  {
+    "revision": "f28cd097101b19ffc8da",
+    "url": "/static/js/2.6aa9a3f5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6aa9a3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b391f5f333b85c75a416",
+    "url": "/static/js/main.a2cb2e31.chunk.js"
+  },
+  {
+    "revision": "0266ed20f0bde903c660",
+    "url": "/static/js/runtime-main.61f118ae.js"
+  },
+  {
+    "revision": "53e2919c30d7c4f2e0e7c9cf42864f7e",
+    "url": "/static/media/logopoe.53e2919c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ca82dbc36c95e4e112af90402da00f4.js"
+  "/precache-manifest.da74e1512f8fdeac163da1aa1b204d94.js"
 );
 
 self.addEventListener('message', (event) => {
